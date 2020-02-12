150eb9f86fb83d8fe6481936f327d7f758a4661b
CÉSAR PARRADO <cesarparrado970@gmail.com>
2020-02-11 19:36:09 -0500

    tarea de prueba

--- worlds/tasks.py
+++ worlds/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task
 
+from worlds.models import World
 from worlds.utils import web_scraping
 
 
 @shared_task
 def scraping():
-    print("Hola!")
+    World.objects.create()
