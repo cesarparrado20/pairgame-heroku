072d901b2424cd91b321c10b21b93698db39bf1f
CÉSAR PARRADO <cesarparrado970@gmail.com>
2020-02-20 13:14:50 -0500

    cambiar tarea de scraping

--- worlds/tasks.py
+++ worlds/tasks.py
@@ -1,9 +1,8 @@
 from celery import shared_task
 
-from worlds.models import World
 from worlds.utils import web_scraping
 
 
 @shared_task
 def scraping():
-    World.objects.create()
+    web_scraping()
